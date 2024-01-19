0ae0f0d109478e613e980993d104f588b0c20bb9
prathibhasolutions <prathibhasolutions1@gmail.com>
2024-01-19 17:49:10 +0530

    Update settings.py

--- commerce/settings.py
+++ commerce/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'commerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
@@ -124,4 +128,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+
+
 django_heroku.settings(locals())
